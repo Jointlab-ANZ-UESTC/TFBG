ee31fd778b3ec8587110c1756deae071a61feaa8
KevinYoung <1072780015@qq.com>
2019-04-20 20:34:53 +0800

    [feat]results showed in html file

---
+++ ticket3.py
@@ -0,0 +1,23 @@
+from PyH.PyH.pyh import *
+
+''' 
+
+前边朋友的code
+
+'''
+
+# 创建HTML页面
+page = PyH("Results for Project 0")
+
+# 创建展示数据表格
+tb = page << table('', border='2', align='center')
+tb << caption('Results for urls(status 200)')
+with open('./results.csv', 'r') as f1:
+    for line in f1:
+        tb << tr(td(line.lstrip()))
+
+#保存html文件
+page.printOut('re.html')
+
+
+
